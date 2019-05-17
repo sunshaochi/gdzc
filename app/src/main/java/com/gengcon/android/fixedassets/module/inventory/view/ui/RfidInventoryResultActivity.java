75eb42f5ca1717ab404b18300d9fbb908971872b
yupengtao <1078941788@qq.com>
2019-05-17 15:31:42 +0800

    rfid盘点页面数据接入

--- app/src/main/java/com/gengcon/android/fixedassets/module/inventory/view/ui/RfidInventoryResultActivity.java
+++ app/src/main/java/com/gengcon/android/fixedassets/module/inventory/view/ui/RfidInventoryResultActivity.java
@@ -6,19 +6,29 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.gengcon.android.fixedassets.R;
+import com.gengcon.android.fixedassets.bean.result.ResultAsset;
 import com.gengcon.android.fixedassets.module.base.BasePullRefreshActivity;
 import com.gengcon.android.fixedassets.module.base.GApplication;
-import com.gengcon.android.fixedassets.module.inventory.view.RfidInventoryView;
+import com.gengcon.android.fixedassets.module.greendao.AssetBean;
+import com.gengcon.android.fixedassets.module.greendao.AssetBeanDao;
+import com.gengcon.android.fixedassets.module.greendao.InventoryBean;
+import com.gengcon.android.fixedassets.module.greendao.InventoryBeanDao;
+import com.gengcon.android.fixedassets.module.inventory.presenter.InventoryResultPresenter;
+import com.gengcon.android.fixedassets.module.inventory.view.InventoryResultView;
 import com.gengcon.android.fixedassets.rfid.BackResult;
 import com.gengcon.android.fixedassets.rfid.GetRFIDThread;
 import com.gengcon.android.fixedassets.rfid.MUtil;
 import com.gengcon.android.fixedassets.rfid.RfidDialog;
+import com.gengcon.android.fixedassets.util.Constant;
 import com.gengcon.android.fixedassets.util.Logger;
+import com.gengcon.android.fixedassets.util.SharedPreferencesUtils;
+import com.gengcon.android.fixedassets.util.ToastUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,11 +37,11 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import cn.trinea.android.common.util.ToastUtils;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import realid.rfidlib.EmshConstant;
 
-public class RfidInventoryResultActivity extends BasePullRefreshActivity implements View.OnClickListener, RfidInventoryView, BackResult {
-    private TextView tv_inventory;
+public class RFIDInventoryResultActivity extends BasePullRefreshActivity implements View.OnClickListener, InventoryResultView, BackResult {
     private GetRFIDThread rfidThread;
     private Timer mTimer = null;
     private TimerTask mTimerTask = null;
@@ -40,15 +50,53 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
     private int currentStatue = -1;
     private boolean isconnet;
 
+    private List<AssetBean> mResultList;
+    private String pd_no;
+    private String pd_name;
+    private int pd_status;
+    private int mPage = 1;
+
+    private TextView tv_title_text, tv_title_status, tv_title_right;
+    private LinearLayout noFinishLayout;
+
+    private InventoryResultPresenter mPresenter;
+
+    private AssetBeanDao assetBeanDao;
+    private InventoryBeanDao inventoryBeanDao;
+    private List<AssetBean> assets;
+    private List<String> asset_ids;
+    private List<String> audit_asset_ids;
+    private String user_id;
+    private int isUpdate;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_rfidinverntor);
-        initView();
+        setContentView(R.layout.activity_rfid_inventory);
+        pd_no = getIntent().getStringExtra(Constant.INTENT_EXTRA_KEY_INVENTORY_ID);
+        pd_name = getIntent().getStringExtra("pd_name");
+        pd_status = getIntent().getIntExtra("pd_status", -1);
+        isUpdate = getIntent().getIntExtra("is_update", -1);
+        user_id = (String) SharedPreferencesUtils.getInstance().getParam(SharedPreferencesUtils.USER_ID, "");
+        if (pd_no == null) {
+            finish();
+            return;
+        }
+        asset_ids = new ArrayList<>();
+        audit_asset_ids = new ArrayList<>();
+        assetBeanDao = GApplication.getDaoSession().getAssetBeanDao();
+        assets = assetBeanDao.queryBuilder()
+                .where(AssetBeanDao.Properties.Pd_no.eq(pd_no)).where(
+                        AssetBeanDao.Properties.User_id.eq(user_id)).list();
+        inventoryBeanDao = GApplication.getDaoSession().getInventoryBeanDao();
+        mResultList = new ArrayList<>();
+        mPresenter = new InventoryResultPresenter();
+        mPresenter.attachView(this);
         rfidThread = GetRFIDThread.getInstance();
         new Thread(rfidThread).start();
         monitorEmsh();
         GetRFIDThread.getInstance().setBackResult(this);
+        initView();
     }
 
     private void monitorEmsh() {
@@ -71,8 +119,159 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
     @Override
     protected void initView() {
         super.initView();
-        tv_inventory = findViewById(R.id.tv_inventory);
-        tv_inventory.setOnClickListener(this);
+        findViewById(R.id.iv_title_left).setOnClickListener(this);
+        tv_title_text = findViewById(R.id.tv_title_text);
+        tv_title_status = findViewById(R.id.tv_title_status);
+        tv_title_right = findViewById(R.id.tv_title_right);
+        noFinishLayout = findViewById(R.id.noFinishLayout);
+        tv_title_text.setText(pd_name);
+        tv_title_right.setOnClickListener(this);
+        findViewById(R.id.syncDataView).setOnClickListener(this);
+        findViewById(R.id.auditView).setOnClickListener(this);
+        findViewById(R.id.pdView).setOnClickListener(this);
+        initPdStatus();
+        if (assets.size() == 0) {
+            mPresenter.showInventoryResult(pd_no, mPage);
+        } else {
+            if (pd_status == 4) {
+                if (isUpdate != 1) {
+                    mPresenter.showInventoryResult(pd_no, mPage);
+                } else {
+                    getFinishedFragment(assets);
+                }
+            } else if (pd_status == 2) {
+                if (isUpdate != 1) {
+                    mPresenter.showInventoryResult(pd_no, mPage);
+                } else {
+                    getNoFinishFragment(assets);
+                }
+            } else {
+                getNoFinishFragment(assets);
+            }
+        }
+    }
+
+    private void initPdStatus() {
+        if (pd_status == 1 || pd_status == 3) {
+            tv_title_status.setText(R.string.inventory_doing);
+            tv_title_status.setBackgroundResource(R.drawable.bg_inventory_doing);
+            tv_title_right.setVisibility(View.VISIBLE);
+            noFinishLayout.setVisibility(View.VISIBLE);
+        } else if (pd_status == 2) {
+            tv_title_status.setText(R.string.inventory_wait);
+            tv_title_status.setBackgroundResource(R.drawable.bg_inventory_wait);
+            tv_title_right.setVisibility(View.GONE);
+            noFinishLayout.setVisibility(View.GONE);
+        } else {
+            tv_title_status.setText(R.string.inventory_finished);
+            tv_title_status.setBackgroundResource(R.drawable.bg_inventory_finished);
+            tv_title_right.setVisibility(View.GONE);
+            noFinishLayout.setVisibility(View.GONE);
+        }
+    }
+
+    private void getNoFinishFragment(List<AssetBean> assetBeans) {
+        FragmentManager fm = this.getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        InventoryNoFinishFragment noFinishFragment = new InventoryNoFinishFragment(assetBeans);
+        ft.replace(R.id.fl, noFinishFragment);
+        ft.commitAllowingStateLoss();
+    }
+
+    private void getFinishedFragment(List<AssetBean> assetBeans) {
+        FragmentManager fm = this.getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        InventoryFinishedFragment finishedFragment = new InventoryFinishedFragment(assetBeans);
+        ft.replace(R.id.fl, finishedFragment);
+        ft.commitAllowingStateLoss();
+    }
+
+    @Override
+    public void showInventoryResult(ResultAsset resultAsset) {
+        if (mPage == 1) {
+            mResultList.clear();
+            mResultList.addAll(resultAsset.getList());
+        } else {
+            mResultList.addAll(resultAsset.getList());
+        }
+        if (mPage < resultAsset.getPage_count()) {
+            mPage++;
+            mPresenter.showInventoryResult(pd_no, mPage);
+        }
+        if (resultAsset.getCurrent_page() == resultAsset.getPage_count()) {
+            for (int i = 0; i < mResultList.size(); i++) {
+                mResultList.get(i).setPd_no(pd_no);
+                mResultList.get(i).setUser_id(user_id);
+            }
+            assetBeanDao.deleteInTx(assetBeanDao.queryBuilder()
+                    .where(assetBeanDao.queryBuilder()
+                            .and(AssetBeanDao.Properties.Pd_no.eq(pd_no),
+                                    AssetBeanDao.Properties.User_id.eq(user_id))).list());
+            assetBeanDao.insertInTx(mResultList);
+            if (pd_status == 4) {
+                getFinishedFragment(mResultList);
+            } else if (pd_status == 1 || pd_status == 3 || pd_status == 2) {
+                getNoFinishFragment(mResultList);
+            }
+        }
+    }
+
+    @Override
+    public void syncAssetSuccess() {
+        asset_ids.clear();
+        ToastUtils.toastMessage(this, "同步数据成功");
+        mPage = 1;
+        mPresenter.showInventoryResult(pd_no, mPage);
+    }
+
+    @Override
+    public void syncAssetFailed(int type, String msg) {
+        ToastUtils.toastMessage(this, msg);
+        if (type == 1) {
+            List<AssetBean> isDeleteAsset = assetBeanDao.queryBuilder().where(AssetBeanDao.Properties.Pd_no.eq(pd_no)).list();
+            assetBeanDao.deleteInTx(isDeleteAsset);
+            finish();
+        } else if (type == 2) {
+            pd_status = 2;
+            initPdStatus();
+            mPage = 1;
+            mPresenter.showInventoryResult(pd_no, mPage);
+        } else if (type == 3) {
+            pd_status = 4;
+            initPdStatus();
+            mPage = 1;
+            mPresenter.showInventoryResult(pd_no, 1);
+        }
+    }
+
+    @Override
+    public void keepSonAuditSuccess() {
+        audit_asset_ids.clear();
+        ToastUtils.toastMessage(this, "提交审核成功");
+        pd_status = 2;
+        initPdStatus();
+        mPage = 1;
+        mPresenter.showInventoryResult(pd_no, mPage);
+    }
+
+    @Override
+    public void keepSonAuditFailed(int type, String msg) {
+        ToastUtils.toastMessage(this, msg);
+        if (type == 1) {
+            List<AssetBean> isDeleteAsset = assetBeanDao.queryBuilder().where(AssetBeanDao.Properties.Pd_no.eq(pd_no)).list();
+            assetBeanDao.deleteInTx(isDeleteAsset);
+            finish();
+        } else if (type == 2) {
+            pd_status = 2;
+            initPdStatus();
+            mPage = 1;
+            mPresenter.showInventoryResult(pd_no, mPage);
+        } else if (type == 3) {
+            pd_status = 4;
+            initPdStatus();
+            mPage = 1;
+            mPresenter.showInventoryResult(pd_no, 1);
+        }
     }
 
     public class EmshStatusBroadcastReceiver extends BroadcastReceiver {
@@ -117,16 +316,71 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.tv_inventory:
+            case R.id.iv_title_left:
+                InventoryBean inventoryUpdate = inventoryBeanDao.queryBuilder()
+                        .where(InventoryBeanDao.Properties.User_id.eq(user_id))
+                        .where(InventoryBeanDao.Properties.Pd_no.eq(pd_no)).unique();
+                long wpCount = assetBeanDao.queryBuilder().where(AssetBeanDao.Properties.User_id.eq(user_id))
+                        .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                        .where(AssetBeanDao.Properties.Pd_status.eq(1)).count();
+                long ypCount = assetBeanDao.queryBuilder().where(AssetBeanDao.Properties.User_id.eq(user_id))
+                        .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                        .where(AssetBeanDao.Properties.Pd_status.eq(2)).count();
+                inventoryUpdate.setWp_num((int) wpCount);
+                inventoryUpdate.setYp_num((int) ypCount);
+                inventoryBeanDao.update(inventoryUpdate);
+                finish();
+                break;
+            case R.id.tv_title_right:
+                Intent intentRemarks = new Intent(this, InventoryRemarksActivity.class);
+                intentRemarks.putExtra(Constant.INTENT_EXTRA_KEY_INVENTORY_ID, pd_no);
+                startActivity(intentRemarks);
+                break;
+            case R.id.syncDataView:
+                List<AssetBean> hasScanAsset = assetBeanDao.queryBuilder()
+                        .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                        .where(AssetBeanDao.Properties.IsScanAsset.eq(1)).list();
+                if (hasScanAsset.size() > 0) {
+                    for (int i = 0; i < hasScanAsset.size(); i++) {
+                        if (!asset_ids.contains(hasScanAsset.get(i).getAsset_id())) {
+                            asset_ids.add(hasScanAsset.get(i).getAsset_id());
+                        }
+                    }
+                }
+                mPresenter.showSyncAssetData(pd_no, asset_ids);
+                break;
+            case R.id.auditView:
+                List<AssetBean> scanAsset = assetBeanDao.queryBuilder()
+                        .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                        .where(AssetBeanDao.Properties.IsScanAsset.eq(1)).list();
+                if (scanAsset.size() > 0) {
+                    for (int i = 0; i < scanAsset.size(); i++) {
+                        if (!audit_asset_ids.contains(scanAsset.get(i).getAsset_id())) {
+                            audit_asset_ids.add(scanAsset.get(i).getAsset_id());
+                        }
+                    }
+                }
+                InventoryBean inventory = inventoryBeanDao.queryBuilder()
+                        .where(InventoryBeanDao.Properties.User_id.eq(user_id))
+                        .where(InventoryBeanDao.Properties.Pd_no.eq(pd_no)).unique();
+                String remarks = inventory.getRemarks();
+                mPresenter.auditAssetData(pd_no, remarks, audit_asset_ids);
+                break;
+            case R.id.pdView:
                 if (isconnet) {
                     startRFID();
                 } else {
-                    ToastUtils.show(RfidInventoryResultActivity.this, "电源无法开启");
+                    ToastUtils.toastMessage(RFIDInventoryResultActivity.this, "电源无法开启");
                 }
-
                 break;
         }
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
     }
 
     /**
@@ -138,7 +392,6 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
             GApplication.getInstance().getIdataLib().startInventoryTag();
             showRfidInventoryingDialog();
             GetRFIDThread.getInstance().setIfPostMsg(true);
-            com.gengcon.android.fixedassets.util.ToastUtils.toastMessage(RfidInventoryResultActivity.this, "开始扫描");
         }
 
     }
@@ -151,15 +404,18 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
         if (flag) {//再扫描
             GApplication.getInstance().getIdataLib().stopInventory();
             GetRFIDThread.getInstance().setIfPostMsg(false);
-            com.gengcon.android.fixedassets.util.ToastUtils.toastMessage(RfidInventoryResultActivity.this, "开始扫描");
         }
 
     }
 
     private void showRfidInventoryingDialog() {
         if (dialog == null) {
-            dialog = new RfidDialog(RfidInventoryResultActivity.this).builder();
-            dialog.setTotal(100);
+            long noFinishAssetCount = assetBeanDao.queryBuilder()
+                    .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                    .where(AssetBeanDao.Properties.User_id.eq(user_id))
+                    .where(AssetBeanDao.Properties.Pd_status.eq(1)).count();
+            dialog = new RfidDialog(RFIDInventoryResultActivity.this).builder();
+            dialog.setTotal((int) noFinishAssetCount);
             dialog.setNum(realDataMap.size() + "");
             dialog.setLeft("暂停");
             dialog.setStopClick(new RfidDialog.StopListener() {
@@ -180,8 +436,8 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
                 @Override
                 public void onClick() {
                     stopRFID();
-                    realDataMap.clear();
-                    realKeyList.clear();
+//                    realDataMap.clear();
+//                    realKeyList.clear();
                     dialog.dismiss();
                     dialog = null;
                 }
@@ -191,6 +447,22 @@ public class RfidInventoryResultActivity extends BasePullRefreshActivity impleme
                 @Override
                 public void onClick() {
                     stopRFID();
+                    if (realKeyList.size() > 0) {
+                        for (int i = 0; i < realKeyList.size(); i++) {
+                            AssetBean asset = assetBeanDao.queryBuilder()
+                                    .where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                                    .where(AssetBeanDao.Properties.User_id.eq(user_id))
+                                    .where(AssetBeanDao.Properties.Asset_id.eq(realKeyList.get(i)))
+                                    .unique();
+                            if (asset != null) {
+                                asset.setPd_status(2);
+                                assetBeanDao.update(asset);
+                            }
+                        }
+                    }
+                    assets = assetBeanDao.queryBuilder().where(AssetBeanDao.Properties.Pd_no.eq(pd_no))
+                            .where(AssetBeanDao.Properties.User_id.eq(user_id)).list();
+                    getNoFinishFragment(assets);
                     realDataMap.clear();
                     realKeyList.clear();
                     dialog.dismiss();
