1525194c3e4e3b645e9a9d74aaed46e7fd21fbc7
yupengtao <1078941788@qq.com>
2019-05-21 11:30:41 +0800

    修复关于精臣固定资产页面点击版本崩溃问题

--- app/src/main/java/com/gengcon/android/fixedassets/module/user/view/ui/AboutUsActivity.java
+++ app/src/main/java/com/gengcon/android/fixedassets/module/user/view/ui/AboutUsActivity.java
@@ -39,6 +39,7 @@ public class AboutUsActivity extends BaseActivity implements View.OnClickListene
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        mUpdateVersionPresenter.getVersion();
         initView();
     }
 
